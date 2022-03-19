e6f3f5506a31b837cb4a8536767da77640ae1179
이해주 <hj_lee_com@naver.com>
2022-03-19 11:51:17 +0900

    test : swift 프로젝트 경로에서 commit이 정상적으로 되는지 테스트

--- emJoo/week1/Dicee/Dicee/ViewController.swift
+++ emJoo/week1/Dicee/Dicee/ViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    /*  Commit TEST !!!!! */
+    lazy var randomNumLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start Game" // initial String value
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.textColor = .white
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
