f49acb3ef31ae0c0e66d37aea13591f821762472
이해주 <hj_lee_com@naver.com>
2022-03-20 17:41:14 +0900

    dev : 게임 승리조건 로직 완성  (showAlert, resetHandler)

--- emJoo/week1/Dicee/Dicee/ViewController.swift
+++ emJoo/week1/Dicee/Dicee/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
     }()
     
     
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +49,7 @@ class ViewController: UIViewController {
         
         // Add target (타겟 메소드 적용)
         rollButton.addTarget(self, action: #selector(tapRollButton), for: .touchUpInside)
+    
         
         
         
@@ -66,7 +67,7 @@ class ViewController: UIViewController {
         buttonStackView.bottomAnchor.constraint(equalTo: rollButton.layoutMarginsGuide.topAnchor, constant: -82).isActive = true
         
         
-    
+        
         // Align Contrains with NS (Align 레이아웃 설정)
         NSLayoutConstraint.activate([
             logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -78,14 +79,20 @@ class ViewController: UIViewController {
     
     
     
-    @objc func tapRollButton () {
+//    // 주사위를 클릭 했을 때
+//    @objc func tapDice () {}
 
-        
-        let generateNum = Int.random(in: 2..<13)
+    
+    
+    // 'Roll' Button을 클릭 했을 때
+    @objc func tapRollButton () {
+                
+        // Genereate Random Int Number (랜덤 숫자 생성)
         let randomDiceNum1 = Int.random(in: 0..<5)
         let randomDiceNum2 = Int.random(in: 0..<5)
         
         if !isPlaying { // 조건: 게임을 진행하고 있지 않다면 == 초기화면 화면 일 때
+            let generateNum = Int.random(in: 2..<13)
             self.mainLabel.text = String(generateNum)
             self.displayNum = generateNum
             
@@ -93,10 +100,7 @@ class ViewController: UIViewController {
             self.dice1.configuration?.image = UIImage(named: ViewController.diceImagePath[randomDiceNum1])
             self.dice2.configuration?.image = UIImage(named: ViewController.diceImagePath[randomDiceNum2])
             
-            
 
-            
-            
             isPlaying = true
             
         } else { // 조건 : 게임을 진행하고 있을 때
@@ -105,9 +109,38 @@ class ViewController: UIViewController {
             self.dice2.configuration?.image = UIImage(named: ViewController.diceImagePath[randomDiceNum2])
             
         }
+
+        // 게임 성공 여부 확인 로직
+        if self.displayNum == randomDiceNum1 + randomDiceNum2 + 2{
+            showAlert()
+        }
         
     }
+    
+    
+    // 조건에 따라 Alert창을 보여줌
+    func showAlert () {
+        // should show alert
+        let alert = UIAlertController(title: "Your Correct", message: "Greate Job", preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "Confirm", style: .cancel) { _ in
+            self.resetHandler()
+        }
+        alert.addAction(confirm)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    // 모든 State Varialbe과 인터페이스 초기값들을 리셋
+    func resetHandler () {
+        self.mainLabel.text = "Start Game"
+        self.displayNum = nil
+        self.isPlaying = false
+        self.dice1.configuration?.image = UIImage(named: "DiceOne")
+        self.dice2.configuration?.image = UIImage(named: "DiceOne")
 
+    }
+    
     
     
 }
