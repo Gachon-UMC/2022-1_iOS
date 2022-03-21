78c4580e9b3b0643591eb95142798dd1dd5c478e
xongjaemin <woals99@gmail.com>
2022-03-21 16:48:35 +0900

    dev : dicee intermediate 완성

--- jaem/week1/Diceee/Diceee/ViewController.swift
+++ jaem/week1/Diceee/Diceee/ViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var rand1: Int = 0 , rand2: Int = 0 // random number for each dice
+    var randAns: Int = 0 // random answer number
     @IBOutlet weak var dice1View: UIImageView!
     @IBOutlet weak var dice2View: UIImageView!
+    @IBOutlet weak var randNumLabel: UILabel!
+    
+    
     
     let diceArr = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     override func viewDidLoad() {
@@ -21,8 +26,22 @@ class ViewController: UIViewController {
 
 
     @IBAction func rollBtnClickListener(_ sender: Any) {
-        dice1View.image = diceArr.randomElement()
-        dice2View.image = diceArr.randomElement()
+        rand1 = Int.random(in: 0...5)
+        rand2 = Int.random(in: 0...5)
+        randAns = Int.random(in: 2...12)
+        dice1View.image = diceArr[rand1]
+        dice2View.image = diceArr[rand2]
+        randNumLabel.text = String(randAns)
+        
+        if((rand1+1)+(rand2+1) == Int(randAns)){let alert = UIAlertController(title: "Correct!", message: "Great Job", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Confirm", style: .default) { [self] action in
+                self.randNumLabel.text = "Start Game"
+                self.dice1View.image = diceArr[0]
+                self.dice2View.image = diceArr[0]
+              })
+            
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 }
 
