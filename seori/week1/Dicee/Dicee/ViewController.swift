47bf538b2d0f903e108198a12469ce5539f65627
EunsuSeo01 <eunsu0613@naver.com>
2022-03-25 03:12:52 +0900

    dev : Dicee refactoring

--- seori/week1/Dicee/Dicee/ViewController.swift
+++ seori/week1/Dicee/Dicee/ViewController.swift
@@ -15,22 +15,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var rollButton: UIButton!
     @IBOutlet weak var mainLabel: UILabel!
     
+    let diceDic = [1:"DiceOne", 2:"DiceTwo", 3:"DiceThree", 4:"DiceFour", 5:"DiceFive", 6:"DiceSix"]
+    var randomNum = 0, diceSum = 0
+    var dice: Dictionary<Int, String>.Element?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    // Roll 버튼 눌렀을 때 실행되는 액션.
     @IBAction func switchTapped(_ sender: Any) {
-        let diceDic = [1:"DiceOne", 2:"DiceTwo", 3:"DiceThree", 4:"DiceFour", 5:"DiceFive", 6:"DiceSix"]
-        
         // 랜덤 숫자 label로 출력.
-        let randomNum = Int.random(in: 2...12)
+        randomNum = Int.random(in: 2...12)
         mainLabel.text = String(randomNum)
         
-        var diceSum = 0
-        // 주사위 이미지 6개의 이름과 값이 담긴 diceDic에서 랜덤으로 하나 가져오는 것.
-        var dice = diceDic.randomElement()
+        diceSum = 0
         
+        // 주사위 이미지 6개의 이름과 값이 담긴 diceDic에서 랜덤으로 하나 가져오는 것.
+        dice = diceDic.randomElement()
         leftDice.image = UIImage(named: dice!.value) // 그 dice의 value 이미지로 변경.
         diceSum += dice!.key  // dice 숫자 합 계산.
         
@@ -38,15 +41,20 @@ class ViewController: UIViewController {
         rightDice.image = UIImage(named: dice!.value) // 그 dice의 value 이미지로 변경.
         diceSum += dice!.key  // dice 숫자 합 계산.
         
-        // 값이 같을 때 alert 띄우기.
+        // main label의 값과 두 주사위의 합이 같은지 확인.
+        checkEqual()
+    }
+    
+    // 값이 같을 때 alert 띄우는 함수.
+    func checkEqual() {
         if (randomNum == diceSum) {
             let alert = UIAlertController(title: "Correct!", message: "Greate Job", preferredStyle: .alert)
             
             // alert 뜨고 confirm 클릭했을 때 실행할 코드.
-            let alertAction = UIAlertAction(title: "Confirm", style: .default) { _ in
-                self.leftDice.image = UIImage(named: diceDic[1]!)
-                self.rightDice.image = UIImage(named: diceDic[1]!)
-                self.mainLabel.text = "Start Game"
+            let alertAction = UIAlertAction(title: "Confirm", style: .default) { [self] _ in
+                leftDice.image = UIImage(named: diceDic[1]!)
+                rightDice.image = UIImage(named: diceDic[1]!)
+                mainLabel.text = "Start Game"
             }
             
             alert.addAction(alertAction)
