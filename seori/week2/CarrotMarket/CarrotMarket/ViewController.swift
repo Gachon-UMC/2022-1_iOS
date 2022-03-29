8e49edb4a5a3c8e0e7b2fe5d2d250c1623505252
EunsuSeo01 <eunsu0613@naver.com>
2022-03-30 03:08:43 +0900

    dev : tableView cell 구성 & heart button 기능 구현

--- seori/week2/CarrotMarket/CarrotMarket/ViewController.swift
+++ seori/week2/CarrotMarket/CarrotMarket/ViewController.swift
@@ -14,10 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftStackView: UIStackView!
     @IBOutlet weak var rightStackView: UIStackView!
     
+    @IBOutlet weak var customTableView: UITableView!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        customTableView.delegate = self
+        customTableView.dataSource = self
         
-        initNavi()
+        customTableView.rowHeight = 150
+
+        // initNavi()
     }
 
     // navigation bar 구성.
@@ -25,6 +34,27 @@ class ViewController: UIViewController {
         self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: leftStackView)
         self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(customView: rightStackView)
     }
+    
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedItems.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let customCell = customTableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as? CustomCell else {
+            preconditionFailure("테이블 뷰 가져오기 실패")
+        }
+
+        customCell.usedItemImage.image = UIImage(named: usedItems[indexPath.row].imagePath)
+
+        customCell.usedItemTitle.text = usedItems[indexPath.row].title
+        customCell.usedItemSubdescription.text = usedItems[indexPath.row].subDescription
+        customCell.usedItemPrice.text = String(usedItems[indexPath.row].price) + "원"
+
+        return customCell
+    }
+}
