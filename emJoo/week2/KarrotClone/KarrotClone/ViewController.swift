187043835dd6b2ab5c675e9e01926854b8c87b4e
이해주 <hj_lee_com@naver.com>
2022-03-30 14:24:04 +0900

    dev : TableView 구현 및 Insets값 설정

--- emJoo/week2/KarrotClone/KarrotClone/ViewController.swift
+++ emJoo/week2/KarrotClone/KarrotClone/ViewController.swift
@@ -9,27 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    /* Subviews */
-    let locationBarButton = AppbarViews().locationBarButton
-    let rightGroupBarButtons = AppbarViews().rightGroupBarButtons
     
-//    /* TableView */
-//    lazy var tableView: UITableView = {
-//        let tableView = UITableView()
-//        
-//    }()
+    /* Subviews  */
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // TableView 마진 레이아웃 (양옆 공백)
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        tableView.frame.inset(by: UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20))
+        tableView.tableHeaderView = UIView()
+        
+        return tableView
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.leftBarButtonItem = locationBarButton
-        self.navigationItem.rightBarButtonItem = rightGroupBarButtons
+        
+        tableView.dataSource = self
     }
     
     override func viewDidLayoutSubviews() {
+        self.navigationItem.leftBarButtonItem = AppbarViews().locationBarButton
+        self.navigationItem.rightBarButtonItem = AppbarViews().rightGroupBarButtons
         self.view.backgroundColor = .white
+        
+        setTableView()
+        
+    }
+    
+    func setTableView() {
+        self.view.addSubview(tableView)
+        tableView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0).isActive = true
+        tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
     }
     
     
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
