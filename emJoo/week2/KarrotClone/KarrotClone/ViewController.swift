e3a92e7f1b2ad2b2420ed5f45e321495a6dd9222
이해주 <hj_lee_com@naver.com>
2022-03-30 14:08:15 +0900

    dev : Appbar Subviews 구현

--- emJoo/week2/KarrotClone/KarrotClone/ViewController.swift
+++ emJoo/week2/KarrotClone/KarrotClone/ViewController.swift
@@ -9,17 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    /* Subviews */
+    let locationBarButton = AppbarViews().locationBarButton
+    let rightGroupBarButtons = AppbarViews().rightGroupBarButtons
     
-
+//    /* TableView */
+//    lazy var tableView: UITableView = {
+//        let tableView = UITableView()
+//        
+//    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.leftBarButtonItem = locationBarButton
+        self.navigationItem.rightBarButtonItem = rightGroupBarButtons
     }
     
     override func viewDidLayoutSubviews() {
         self.view.backgroundColor = .white
     }
-
-
+    
+    
 }
 
