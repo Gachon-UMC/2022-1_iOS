27c4270c7592a4f6c13decd5efbfe3cdf1612f6b
이해주 <hj_lee_com@naver.com>
2022-03-30 13:35:42 +0900

    dev : Ukit without storyboard 환경

--- emJoo/week2/KarrotClone/KarrotClone/ViewController.swift
+++ emJoo/week2/KarrotClone/KarrotClone/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.view.backgroundColor = .white
     }
 
 
