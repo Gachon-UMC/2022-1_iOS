e804d15959cc720858ace27171a878d882469920
EunsuSeo01 <eunsu0613@naver.com>
2022-04-03 23:14:37 +0900

    Week3 : 오류 수정 & 회원가입 정보 전달 로직 추가

--- seori/week3/Catstagram/Catstagram/LoginViewController.swift
+++ seori/week3/Catstagram/Catstagram/LoginViewController.swift
@@ -11,27 +11,53 @@ class LoginViewController: UIViewController {
 
     var email = String()
     var password = String()
+    var userInfo: UserInfo?
     
-    @IBOutlet weak var popToSignupButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setAttributesToButton()
+        setupAttribute()
     }
     
     @IBAction func emailTextFieldEditingChanged(_ sender: UITextField) {
         // Optional을 해지.
         // ?? "" -> sender.text가 nil이면 ""로 초기화 해주겠다는 뜻.
-        email = sender.text ?? ""
+        let text = sender.text ?? ""
+        
+        self.loginButton.backgroundColor = text.isValidEmail() ? .facebookColor : .disabledButtonColor
+        self.email = text
     }
     
     @IBAction func passwordTextFieldEditingChanged(_ sender: UITextField) {
-        password = sender.text ?? ""
+        let text = sender.text ?? ""
+    
+        self.loginButton.backgroundColor = text.count > 2 ? .facebookColor : .disabledButtonColor
+        self.password = text
     }
     
     @IBAction func loginButtonDidTap(_ sender: UIButton) {
-    
+        // 회원가입 정보를 전달 받아서, 그것과 textField 데이터가 일치하면,
+        // 로그인이 되어야 한다.
+        
+        // guard let을 이욯한 Optional 해제 방법.
+        // 데이터가 있으면(nil이 아니면) 데이터를 선언, 없으면(nil이면) else 이후 코드 실행.
+        guard let userInfo = self.userInfo else { return }
+        
+        if userInfo.email == self.email && userInfo.password == self.password {
+            // 카테고리가 다른 두 화면을 보여줄 때 TabBar를 사용한다.
+            let vc = storyboard?.instantiateViewController(withIdentifier: "TabBarVC") as! UITabBarController
+            
+            // 꽉찬 화면으로 변경.
+            vc.modalPresentationStyle = .fullScreen
+            
+            self.present(vc, animated: true, completion: nil)
+            
+        } else {
+            
+        }
     }
     
     @IBAction func registerButtonDidTap(_ sender: UIButton) {
@@ -48,24 +74,28 @@ class LoginViewController: UIViewController {
         // 오른쪽에서 왼쪽으로 화면이 나오는 방법
         // 더 상세한 내용으로(수직적인 구조) 넘어갈 때 네비게이션 컨트롤러를 주로 사용한다.
         self.navigationController?.pushViewController(registerViewController, animated: true)
+        
+        // 클로저를 통해 회원가입 데이터를 수신.
+        // ARC -> 강한 참조 / 약한 참조
+        // [weak self] -> 약한 참조 -> ARC를 낮춰줌
+        registerViewController.userInfo = { [weak self] (userInfo) in
+            self?.userInfo = userInfo
+        }
     }
     
-    // 버튼에 속성(글자마다 색을 다르게)을 주는 함수.
-    func setAttributesToButton() {
-        // 버튼 안의 텍스트의 폰트 사이즈 설정.
-        let fontSize = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]
+    // 버튼에 속성을 설정하는 함수.
+    func setupAttribute() {
+        let text1 = "계정이 없으신가요?"
+        let text2 = "가입하기"
         
-        // 버튼의 titleLable을 NSMutableAttributedString으로 바꿔서 attributedStr에 저장한다.
-        // & 폰트 사이즈 속성 추가.
-        let attributedStr = NSMutableAttributedString(string: (popToSignupButton.titleLabel?.text)!, attributes: fontSize)
+        let font1 = UIFont.systemFont(ofSize: 13)
+        let font2 = UIFont.boldSystemFont(ofSize: 13)
         
+        let color1 = UIColor.darkGray
+        let color2 = UIColor.facebookColor
         
-        // text중 계정이 없으신가요? 에 해당하는 곳에 Light Gray 색을 준다.
-        attributedStr.addAttribute(.foregroundColor, value: UIColor.lightGray, range: ((popToSignupButton.titleLabel?.text)! as NSString).range(of: "계정이 없으신가요?"))
-
-        attributedStr.addAttribute(.foregroundColor, value: UIColor.init(named: "facebookColor")!, range: ((popToSignupButton.titleLabel?.text)! as NSString).range(of: "가입하기"))
+        let attributes = generateButtonAttribute(self.registerButton, texts: text1, text2, fonts: font1, font2, colors: color1, color2)
         
-        // 속성을 버튼에 적용시킨다.
-        popToSignupButton.setAttributedTitle(attributedStr, for: .normal)
+        self.registerButton.setAttributedTitle(attributes, for: .normal)
     }
 }
--- seori/week3/Catstagram/Catstagram/RegisterViewController.swift
+++ seori/week3/Catstagram/Catstagram/RegisterViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     // MARK: - Properties
-    @IBOutlet weak var bottomButton: UIButton!
-    @IBOutlet weak var signupButton: UIButton!
+    // text field에서 입력한 값을 멤버 변수로 받는다.
+    var email: String = ""
+    var name: String = ""
+    var nickName: String = ""
+    var password: String = ""
+    
+    // 클로저 선언.
+    var userInfo: ((UserInfo) -> Void)?
     
     // 유효성 검사를 위한 프로퍼티.
     var isValidEmail = false {
@@ -37,6 +43,9 @@ class RegisterViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var popToLoginButton: UIButton!
+    
     // TextFields.
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
@@ -52,8 +61,8 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setAttributesToButton()
         setupTextField()
+        setupAttribute()
         
         // bug fix -> 스와이프를 통한 뒤로가기.
         self.navigationController?.interactivePopGestureRecognizer?.delegate = nil
@@ -69,15 +78,19 @@ class RegisterViewController: UIViewController {
         switch sender {
         case emailTextField:
             self.isValidEmail = text.isValidEmail()
+            self.email = text
             
         case nameTextField:
             self.isValidName = text.count > 2
+            self.name = text
         
         case nickNameTextField:
             self.isValidNickName = text.count > 2
+            self.nickName = text
             
         case passwordTextField:
             self.isValidPassword = text.isValidPassword()
+            self.password = text
         
         default:
             fatalError("Missing TextField...")
@@ -89,28 +102,19 @@ class RegisterViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
+    @IBAction func signupButtonDidTap(_ sender: Any) {
+        // 이전화면으로 돌아가기 (뒤로가기)
+        self.navigationController?.popViewController(animated: true)
+        
+        // 구조체 생성.
+        let userInfo = UserInfo(email: self.email, name: self.name, nickName: self.nickName, password: self.password)
+        
+        // 클로저를 통해 RegisterVC(컨트롤러)의 데이터를 전송.
+        self.userInfo?(userInfo)
+    }
     
     
     // MARK: - Helpers
-    // 버튼에 속성(글자마다 색을 다르게)을 주는 함수.
-    func setAttributesToButton() {
-        // 버튼 안의 텍스트의 폰트 사이즈 설정.
-        let fontSize = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]
-        
-        // 버튼의 titleLable을 NSMutableAttributedString으로 바꿔서 attributedStr에 저장한다.
-        // & 폰트 사이즈 속성 추가.
-        let attributedStr = NSMutableAttributedString(string: (bottomButton.titleLabel?.text)!, attributes: fontSize)
-
-        // text중 계정이 있으신가요? 에 해당하는 곳에 Light Gray 색을 준다.
-        attributedStr.addAttribute(.foregroundColor, value: UIColor.lightGray, range: ((bottomButton.titleLabel?.text)! as NSString).range(of: "계정이 있으신가요?"))
-
-        // text중 '로그인'에 해당하는 곳에 facebookColor를 준다.
-        attributedStr.addAttribute(.foregroundColor, value: UIColor.init(named: "facebookColor")!, range: ((bottomButton.titleLabel?.text)! as NSString).range(of: "로그인"))
-        
-        // 속성을 버튼에 적용시킨다.
-        bottomButton.setAttributedTitle(attributedStr, for: .normal)
-    }
-    
     // Text Field와 Action을 연결하는 함수.
     func setupTextField() {
         // 코드의 불필요한 중복을 없애기 위해 연산 프로퍼티와 forEach문을 사용하여 처리.
@@ -120,18 +124,34 @@ class RegisterViewController: UIViewController {
         }
     }
     
+    // 버튼에 속성을 설정하는 함수.
+    func setupAttribute() {
+        let text1 = "계정이 있으신가요?"
+        let text2 = "로그인"
+        
+        let font1 = UIFont.systemFont(ofSize: 13)
+        let font2 = UIFont.boldSystemFont(ofSize: 13)
+        
+        let color1 = UIColor.darkGray
+        let color2 = UIColor.facebookColor
+        
+        let attributes = generateButtonAttribute(self.popToLoginButton, texts: text1, text2, fonts: font1, font2, colors: color1, color2)
+        
+        self.popToLoginButton.setAttributedTitle(attributes, for: .normal)
+    }
+    
     // 사용자가 입력한 회원정보를 확인하고 Valid하면 -> UI 업데이트 (가입 버튼 색 변경)
     private func validateUserInfo() {
         if isValidEmail && isValidName && isValidNickName && isValidPassword {
             
             self.signupButton.isEnabled = true
             UIView.animate(withDuration: 0.33) {    // 애니메이션을 추가.
-                self.signupButton.backgroundColor = UIColor.facebookColor
+                self.signupButton.backgroundColor = .facebookColor
             }
         } else {
             self.signupButton.isEnabled = false     // 버튼의 클릭이 불가능하게.
             UIView.animate(withDuration: 0.33) {
-                self.signupButton.backgroundColor = UIColor.disabledButtonColor
+                self.signupButton.backgroundColor = .disabledButtonColor
             }
         }
     }
@@ -142,7 +162,7 @@ class RegisterViewController: UIViewController {
 extension String {
     // @를 포함하고 2글자 이상일 때 -> True
     func isValidEmail() -> Bool {
-        let emailRegEx = " [A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate.init(format: "SELF MATCHES %@", emailRegEx)
         
         return emailTest.evaluate(with: self)   // 여기서 self는 해당 String을 말한다.
