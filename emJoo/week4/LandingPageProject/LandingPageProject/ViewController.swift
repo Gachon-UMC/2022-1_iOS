b382508b1105a78635bf1347c87067ac85359ec0
이해주 <hj_lee_com@naver.com>
2022-04-11 13:35:24 +0900

    dev : No StoryBoard Setting

---
+++ emJoo/week4/LandingPageProject/LandingPageProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LandingPageProject
+//
+//  Created by 이해주 on 2022/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        view.backgroundColor = .white
+    }
+
+
+}
+
