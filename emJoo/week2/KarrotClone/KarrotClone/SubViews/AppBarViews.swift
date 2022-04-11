0c009b318558fb2eb10b04605a34e2bd41d073dc
이해주 <hj_lee_com@naver.com>
2022-04-11 13:10:58 +0900

    conflict 파일 복구

---
+++ emJoo/week2/KarrotClone/KarrotClone/SubViews/AppBarViews.swift
@@ -0,0 +1,66 @@
+//
+//  AppBarViews.swift
+//  KarrotClone
+//
+//  Created by 이해주 on 2022/03/30.
+//
+
+import UIKit
+
+class AppbarViews {
+    
+    /* left Bar Custom Button */
+    let locationBarButton: UIBarButtonItem = {
+        /* Location Label */
+        let locationLabel = UILabel()
+        locationLabel.text = "역삼동"
+        locationLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        
+        /* Location Icon Button */
+        var buttonConfig = UIButton.Configuration.plain()
+        buttonConfig.image = UIImage(systemName: "arrowtriangle.down.circle")
+        buttonConfig.baseForegroundColor = .black
+        buttonConfig.buttonSize = .small
+        let locationBtn = UIButton(configuration: buttonConfig, primaryAction: nil)
+        
+        let stackView = UIStackView(arrangedSubviews: [locationLabel, locationBtn])
+        let barButton = UIBarButtonItem(customView: stackView)
+        return barButton
+    }()
+    
+    /* Right Bar Custom Button */
+    let rightGroupBarButtons: UIStackView = {
+        var searchConfig = UIButton.Configuration.plain()
+        var heartConfig = UIButton.Configuration.plain()
+        var bellConfig = UIButton.Configuration.plain()
+        
+        searchConfig.image = UIImage(systemName: "magnifyingglass")
+        heartConfig.image = UIImage(systemName: "heart")
+        bellConfig.image = UIImage(systemName: "bell")
+        
+        searchConfig.baseForegroundColor = .black
+        heartConfig.baseForegroundColor = .black
+        bellConfig.baseForegroundColor = .black
+        
+        searchConfig.buttonSize = .small
+        searchConfig.buttonSize = .small
+        bellConfig.buttonSize = .small
+        
+        let serachIconBtn = UIButton(configuration: searchConfig, primaryAction: nil)
+        var heartIconBtn = UIButton(configuration: heartConfig, primaryAction: nil)
+        let bellIconBtn = UIButton(configuration: bellConfig, primaryAction: nil)
+        
+        heartIconBtn.tag = 1
+
+        
+       
+        let stackView = UIStackView(arrangedSubviews: [serachIconBtn, heartIconBtn, bellIconBtn])
+        
+        let barButton = UIBarButtonItem(customView: stackView)
+        return stackView
+    }()
+    
+
+
+    
+}
---
+++ emJoo/week2/KarrotClone/KarrotClone/ViewControllers/FavoriteViewController.swift
@@ -0,0 +1,115 @@
+//
+//  FavoriteViewController.swift
+//  KarrotClone
+//
+//  Created by 이해주 on 2022/03/30.
+//
+
+import Foundation
+import UIKit
+
+class FavoriteViewController: UIViewController {
+    
+    /* Dummy Data & State Variables */
+    
+    var delegate: ReceiveDataDelegate! // Delegate 연동
+    var originItems: [UsedItemModel.UsedItem] = [] // 이전 ViewController 전달할 값.
+    var usedItems: [UsedItemModel.UsedItem] = [] // 해당 ViewController의 Tableview에서 보여줄 값
+    
+    /* Subviews */
+    
+    lazy var backArrowButton: UIBarButtonItem = {
+        let barBtnItem = UIBarButtonItem(image: UIImage(systemName: "arrow.left"), style: .done, target: self, action: #selector(routeBack))
+        barBtnItem.tintColor = .white
+        return barBtnItem
+    }()
+    
+    
+    lazy var tableView = UsedItemTableView().tableView
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.leftBarButtonItem = backArrowButton
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        originItems = usedItems
+        
+        filterUsedItems()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.view.backgroundColor = .white
+        self.title = "관심목록"
+        self.view.addSubview(tableView)
+        
+        setTableView()
+    }
+    
+    
+    func setTableView() {
+        self.view.addSubview(tableView)
+        tableView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0).isActive = true
+        tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
+    }
+    
+    // MARK: INTENT
+    
+    // Route Back 메소드
+    @objc func routeBack() {
+        delegate.receiveChildData(self, data: originItems) // 이전 ViewController로 값 전달
+        _ = navigationController?.popToRootViewController(animated: true) // 이전 View Controller로 이동
+
+    }
+    
+    // usedItems 리스트 중 isLiked 값이 false인 값만 필터링
+    func filterUsedItems() {
+        let filteredItems = usedItems.filter { item in
+            item.isLiked == true
+        }
+        usedItems = filteredItems
+    }
+    
+
+}
+
+
+extension FavoriteViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "usedItemCell", for: indexPath) as! UsedItemTableViewCell
+        let selectedItem = usedItems[indexPath.row]
+        let formatter = NumberFormatter()
+        formatter.numberStyle = NumberFormatter.Style.decimal
+        
+        cell.titleLabel.text = selectedItem.title
+        cell.thumnail.image = UIImage(named: selectedItem.imagePath)
+        cell.locationLabel.text = selectedItem.subDescription
+        cell.priceLabel.text = formatter.string(for: selectedItem.price)
+        cell.heartIcon.image = selectedItem.isLiked ? UIImage(systemName: "heart.fill") : UIImage(systemName: "heart")
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = usedItems[indexPath.row]
+        originItems[selectedItem.id].isLiked = !usedItems[indexPath.row].isLiked
+        usedItems.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
+
+
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let responsiveCellHeight = self.view.frame.height * CGFloat(0.19)
+        return responsiveCellHeight
+    }
+    
+}
