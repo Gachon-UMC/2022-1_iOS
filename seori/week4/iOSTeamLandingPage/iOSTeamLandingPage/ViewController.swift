5e5863a481c09dab93f88c6481ce49ef3c2bfba6
EunsuSeo01 <eunsu0613@naver.com>
2022-04-13 14:13:28 +0900

    dev : image sets 추가 & navigation bar 설정

--- seori/week4/iOSTeamLandingPage/iOSTeamLandingPage/ViewController.swift
+++ seori/week4/iOSTeamLandingPage/iOSTeamLandingPage/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // navigation bar item으로 넣을 이미지 설정. (umc 로고)
+    var umcImage: UIImageView = {
+        let umcImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 36, height: 36))
+        umcImage.image = UIImage(named: "image_1")
+        umcImage.contentMode = .scaleAspectFit
+        return umcImage
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // navigation bar 세팅.
+        setNavigationBar()
     }
 
-
+    // navigation bar 세팅.
+    func setNavigationBar() {
+        // leftBarButtonItem을 umcImage로 설정.
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: umcImage)
+        
+        // 색깔 설정.
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.backgroundColor = .black
+        self.navigationController?.navigationBar.standardAppearance = navigationBarAppearance
+        // 스크롤 했을 때도 색깔이 똑같이 black으로 보이도록 설정.
+        self.navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+    }
 }
 
