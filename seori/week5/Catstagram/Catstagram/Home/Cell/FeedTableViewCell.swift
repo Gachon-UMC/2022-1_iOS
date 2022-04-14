f0c3b7adcc640b3d6f501f2456db365c666ec2da
EunsuSeo01 <eunsu0613@naver.com>
2022-04-15 03:20:42 +0900

    Week5 : nib 파일로 셀 내용 구성

---
+++ seori/week5/Catstagram/Catstagram/Home/Cell/FeedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedTableViewCell.swift
+//  Catstagram
+//
+//  Created by 서은수 on 2022/04/15.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
