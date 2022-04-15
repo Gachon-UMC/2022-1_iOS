d3e079a43d3f0122f174a0ad78b69668e3b0cb1f
EunsuSeo01 <eunsu0613@naver.com>
2022-04-16 03:50:04 +0900

    Week5 : nib 파일 적용, 셀 스타일 설정

--- seori/week5/Catstagram/Catstagram/Home/HomeViewController.swift
+++ seori/week5/Catstagram/Catstagram/Home/HomeViewController.swift
@@ -16,6 +16,13 @@ class HomeViewController: UIViewController {
 
         tableView.delegate = self
         tableView.dataSource = self
+        // cell의 구분선을 제거.
+        tableView.separatorStyle = .none
+        
+        // Nib 파일을 셀로 등록.
+        let feedNib = UINib(nibName: "FeedTableViewCell", bundle: nil)
+        tableView.register(feedNib, forCellReuseIdentifier: "FeedTableViewCell")
+        
     }
 
 }
@@ -29,8 +36,16 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     // 어떠한 cell을 보여줄 것인지 설정.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.backgroundColor = .blue
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as? FeedTableViewCell else {
+            return UITableViewCell()    // else -> 기본 셀을 반환.
+        }
+        // cell이 선택됐을 때 색깔 바뀌는 효과 제거.
+        cell.selectionStyle = .none
         return cell
+    }
+    
+    // cell의 높이 설정.
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 550
     }
 }
