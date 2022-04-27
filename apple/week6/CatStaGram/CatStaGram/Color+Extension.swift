8d45eb7494169f955a5ae4135488291583fa05d0
Jodongjin <ehdwlsdlwkd@naver.com>
2022-04-27 22:53:35 +0900

    add week6

---
+++ apple/week6/CatStaGram/CatStaGram/Color+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  Color+Extension.swift
+//  CatStaGram
+//
+//  Created by 조동진 on 2022/03/26.
+//
+
+import UIKit
+
+extension UIColor {
+  static let facebookColor = UIColor(
+    red: 0.255,
+    green: 0.576,
+    blue: 0.947,
+    alpha: 1.0)
+  
+  static let disabledButtonColor = UIColor(
+    red: 0.776,
+    green: 0.872,
+    blue: 0.980,
+    alpha: 1.0)
+}
