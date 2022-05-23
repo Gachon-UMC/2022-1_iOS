c268898c5ed486f3c5286e7f8e996b8f4291d1c9
EunsuSeo01 <eunsu0613@naver.com>
2022-05-23 17:07:07 +0900

    MemorizeGame - initial commit

---
+++ seori/week10/MemorizeGame/MemorizeGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemorizeGame
+//
+//  Created by 서은수 on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
